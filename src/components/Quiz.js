72b1306c1a207f7f7234677a00a1c35a25f37922
jamesabustan <jamesabustan04@gmail.com>
2020-04-27 13:50:03 +0100

    basic styling added

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from './Quiz.module.css';
 
 class Quiz extends React.Component {
     render() {
       return (
-      <div>
-      <h3>Question : {this.props.questions}</h3>
+      <div className={styles.quizDiv}>
+      <h3 className={styles.question}>Question : {this.props.questions}</h3>
       </div>
       )
     }
