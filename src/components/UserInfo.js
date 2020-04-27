b32b8faf91c1bc266dc446587198987b44b8a6ae
Rosie Phelan <rosiephelan@hotmail.co.uk>
2020-04-27 12:46:40 +0100

    form console.logging input and setting state within userinfo

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -2,25 +2,55 @@ import React from 'react';
 
 
 class UserInfo extends React.Component{
-    render() {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            username: '',
+            difficulty: ''
+        }
+    }
+
+    getUserData = (event) => {
+        event.preventDefault();
+        this.setState({
+            username: event.target[0].value
+        })
+
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            difficulty: event.target.value
+        });
+    }
+
+    componentDidUpdate() {
+        console.log(this.state);
+    }
+    
+
+
+    render() {
+        
         return( 
             <div>
-                <form>
-                    <label for="username">Username:</label>
-                    <input type="text" name="username" id="username" />
+                <form onSubmit={this.getUserData}>
+                    <label>Username:</label>
+                    <input type="text" name="username" id="username" onChange={this.handleChange} />
 
                     <p>Level of Difficulty:</p>
-                    <label for="easy">Easy</label>
-                    <input type="radio" name="difficulty" id="easy" value="Easy" />
-                    <label for="medium">Medium</label>
-                    <input type="radio" name="difficulty" id="medium" value="Medium" />
-                    <label for="difficult">Difficult</label>
-                    <input type="radio" name="difficulty" id="difficult" value="Difficult" />
+                    <label>Easy</label>
+                    <input type="radio" name="difficulty" id="easy" value="easy" checked={this.state.difficulty === "easy"} onChange={this.handleChange} />
+                    <label>Medium</label>
+                    <input type="radio" name="difficulty" id="medium" value="medium" checked={this.state.difficulty === "medium"} onChange={this.handleChange} />
+                    <label>Difficult</label>
+                    <input type="radio" name="difficulty" id="difficult" value="difficult" checked={this.state.difficulty === "difficult"} onChange={this.handleChange}/>
 
                     {/* dropdown for categories */}
 
-                    <button type="submit">Start Quiz</button>
+                    <input type="submit" />
                 </form>
             </div>
 
