198f7ab929c73912ae5c044fece6c5aae1aae016
Rosie Phelan <rosiephelan@hotmail.co.uk>
2020-04-27 11:48:29 +0100

    form set up and saved

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -5,7 +5,24 @@ class UserInfo extends React.Component{
     render() {
 
         return( 
-            <div>UserInfo</div>
+            <div>
+                <form>
+                    <label for="username">Username:</label>
+                    <input type="text" name="username" id="username" />
+
+                    <p>Level of Difficulty:</p>
+                    <label for="easy">Easy</label>
+                    <input type="radio" name="difficulty" id="easy" value="Easy" />
+                    <label for="medium">Medium</label>
+                    <input type="radio" name="difficulty" id="medium" value="Medium" />
+                    <label for="difficult">Difficult</label>
+                    <input type="radio" name="difficulty" id="difficult" value="Difficult" />
+
+                    {/* dropdown for categories */}
+
+                    <button type="submit">Start Quiz</button>
+                </form>
+            </div>
 
         )
     }
