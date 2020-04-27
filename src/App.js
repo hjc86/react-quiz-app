270cd0bbc1341011debdd488935e69bee8dfc604
jamesabustan <jamesabustan04@gmail.com>
2020-04-27 10:36:28 +0100

    Initial set up

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import UserInfo from './components/UserInfo';
+import Quiz from './components/Quiz';
+import Score from './components/Score';
+import './App.css';
+
+
+function App() {
+
+// states GetUsername, GetAPI, GetQuestions, GetAnswers, GetScore
+
+// GetAPI function
+
+// GetQuestions function
+
+// GetAnswers function
+
+// GetScore function
+
+  return (
+    <div className="container">
+      <UserInfo />
+      <Quiz />
+      <Score />
+    </div>
+  );
+}
+
+export default App;
