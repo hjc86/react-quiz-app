d073d945b90210e0666deaf9901ee0c7e178c704
Rosie Phelan <rosiephelan@hotmail.co.uk>
2020-04-30 10:20:20 +0100

    working on scores

--- src/components/Score.js
+++ src/components/Score.js
@@ -13,18 +13,24 @@ class Score extends React.Component{
         }
     }
     
-    checkWinner = (winner) => {
-        if (this.props.scoreArray[0] > this.props.scoreArray[1])
+    checkWinner = async (winner) => {
+        if ((this.props.scoreArray[0] > this.props.scoreArray[1] ) &&
+            (this.props.scoreArray[0] > this.props.scoreArray[2]) &&
+            (this.props.scoreArray[0] > this.props.scoreArray[3]) &&
+            (this.props.scoreArray[0] > this.props.scoreArray[4]) )
         {
             console.log('user1 won', winner);
 
             this.setState({
                 winner: this.props.userName[0]
             })
+        // } else {
+        //     this.setState({
+        //         winner: this.props.userName[1]
+        //     })
         }
     }
 
-   
     componentDidMount(){
         console.log("score updated ")
         this.checkWinner();
@@ -39,14 +45,19 @@ class Score extends React.Component{
         console.log("username arary", this.props.userNameArray)
         console.log("winner", this.state.winner)
 
-
         return(
             <div> 
-                <h1>The winner is: </h1>
+                <h1>The winner is: {this.state.winner}</h1>
 
                 <p>{this.props.userName[0]} scored {this.props.scoreArray[0]}/5!</p>
             
                 <p>{this.props.userName[1]} scored {this.props.scoreArray[1]}/5!</p> 
+
+                <p>{this.props.userName[2]} scored {this.props.scoreArray[2]}/5!</p>
+
+                <p>{this.props.userName[3]} scored {this.props.scoreArray[3]}/5!</p>
+
+                <p>{this.props.userName[4]} scored {this.props.scoreArray[4]}/5!</p>
 
                 <div className={styles.startButtonDiv}>
                 <button className={styles.startButton} onClick={this.refreshPage}>Start New Quiz!</button>
