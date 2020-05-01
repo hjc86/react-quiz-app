c98c22f01b5e55286cffb341f07d0eed00b76739
Helder Carmen <heldercarmen@gmail.com>
2020-05-01 13:02:24 +0100

    change initial state of catergory in UserInfo to undefined

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -11,7 +11,7 @@ class UserInfo extends React.Component{
         this.state = {
             userName : null,
             difficulty: "",
-            category: "",
+            category: undefined,
             categoryName: ""
         }
    }
